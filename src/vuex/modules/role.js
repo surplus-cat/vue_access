4b9429094393b264b3cbd6643a747b553cf06f61
Yang <1073406997@qq.com>
2019-04-11 08:51:39 +0800

    细节修复

--- src/vuex/modules/role.js
+++ src/vuex/modules/role.js
@@ -1,5 +1,5 @@
 import store from '../index'
-import { fullPath } from '../../router/index'
+import router from '../../router/index'
 export default {
   state: {
     info: '' // 每次刷新都要通过token请求个人信息来筛选动态路由
@@ -20,14 +20,14 @@ export default {
       }
       sessionStorage.setItem('info', JSON.stringify(store.getters.info))
       store.dispatch('newRoutes', options.role)
-      fullPath.addRoutes(store.getters.addRouters)
+      router.addRoutes(store.getters.addRouters)
     }
   },
   actions: {
-    getInfo ({commit}, token) {
+    getInfo ({ commit }, token) {
       commit('getInfo', token)
     },
-    setRole ({commit}, options) {
+    setRole ({ commit }, options) {
       // 权限测试
       commit('setRole', options)
     }
